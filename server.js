f873ee46d302c6b83282d3a44bf2a32e4b6c2843
TheOneDV <dvofficial.cam@gmail.com>
2022-08-27 22:14:00 +0700

    Signin and Register Backend

--- server.js
+++ server.js
@@ -1,13 +1,60 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json())
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Dv',
+            email: 'dv@gmail.com',
+            password: 'Theone',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Andreu',
+            email: 'andrei@gmail.com',
+            password: 'john',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
+
 app.get('/', (req, res) => {
-    res.send('This is working')
+    console.log('Work')
+})
+
+app.post('/signin', (req, res) => {
+    if (req.users.email === database.users[0].email &&
+        req.users.password === database.users[0].password) {
+            res.json('success')
+        } else {
+            res.status(400).json('Error Logging in')
+        }
+    res.json('Signin Success')
 })
 
 app.listen(3000, () => {
     console.log('App is runing!')
+})
+
+app.post('/register', (req, res) => {
+    const {email, name, password} = req.body;
+    database.users,push({
+        id: '123',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
 })
 
 
