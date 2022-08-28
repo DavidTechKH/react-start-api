87d5f3dee6cd15f89896d72ee8e5b07ec8bc61b4
David Tech <davidtech@gmail.com>
2022-08-28 17:51:33 +0700

    update

--- server.js
+++ server.js
@@ -1,66 +1,117 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  // Enter your own database information here based on what you created
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'qifwmmvumkxqupum_david-tech',
+    password : '',
+    database : 'qifwmmvumkxqupum_davidtech'
+  }
+});
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'Dv',
-            email: 'dv@gmail.com',
-            password: 'Theone',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Andreu',
-            email: 'andrei@gmail.com',
-            password: 'john',
-            entries: 0,
-            joined: new Date()
-        },
-    ]
-}
 
-app.get('/', (req, res) => {
-    console.log('Work')
+app.get('/', (req, res)=> {
+  res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-    if (req.users.email === database.users[0].email &&
-        req.users.password === database.users[0].password) {
-            res.json('success')
-        } else {
-            res.status(400).json('Error Logging in')
-        }
-    res.json('Signin Success')
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+            // loginEmail[0] --> this used to return the email
+            // TO
+            // loginEmail[0].email --> this now returns the email
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+    // entries[0] --> this used to return the entries
+    // TO
+    // entries[0].entries --> this now returns the entries
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, () => {
     console.log('App is runing!')
 })
 
-app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    database.users,push({
-        id: '123',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
-})
 
 
 
-
-/*
+/* PLan for API todos:
 / --> res = this is working
 /signin --> POST = success/fail
 /register --> POST = user
